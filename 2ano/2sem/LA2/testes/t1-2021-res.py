0a65239b69a4271d3e7ae32633ca7937ff55a3a9
David81820 <64503931+David81820@users.noreply.github.com>
2022-03-04 13:00:20 +0000

    Create t1-2021-res.py

---
+++ 2ano/2sem/LA2/testes/t1-2021-res.py
@@ -0,0 +1,102 @@
+#------------------------------------------------------------------------------------------------------
+#   FORMATA
+#------------------------------------------------------------------------------------------------------
+
+"""
+Implemente uma função que formata um programa em C.
+O código será fornecido numa única linha e deverá introduzir
+um '\n' após cada ';', '{', ou '}' (com excepção da última linha).
+No caso do '{' as instruções seguintes deverão também estar identadas
+2 espaços para a direita.
+"""
+
+def formata(codigo):
+    ind = 0
+    i = 0
+    l = len(codigo)
+    final = ""
+    newline = True
+    for c in codigo:
+        i += 1
+        if c == ';':
+            final += c + ('\n'+ind*' ')*(i<l)
+            newline = True
+        elif c == '{':
+            ind += 2
+            final += c + ('\n'+ind*' ')*(i<l)
+            newline = True
+        elif c == '}':
+            final = final[:-2]
+            ind -= 2
+            final += c + ('\n'+ind*' ')*(i<l)
+            newline = True
+        elif c == ' ' and newline:
+            continue
+        else:
+            newline = False
+            final += c
+    
+    return final
+
+
+
+#------------------------------------------------------------------------------------------------------
+#   HORÁRIO
+#------------------------------------------------------------------------------------------------------
+
+"""
+Implemente uma função que calcula o horário de uma turma de alunos.
+A função recebe dois dicionários, o primeiro associa a cada UC o
+respectivo horário (um triplo com dia da semana, hora de início e
+duração) e o segundo associa a cada aluno o conjunto das UCs em
+que está inscrito. A função deve devolver uma lista com os alunos que
+conseguem frequentar todas as UCs em que estão inscritos, indicando
+para cada um desses alunos o respecto número e o número total de horas
+semanais de aulas. Esta lista deve estar ordenada por ordem decrescente
+de horas e, para horas idênticas, por ordem crescente de número.
+"""
+
+def calcHoras(inscr, ucs):
+    horas = 0
+    
+    for cadeira in inscr:
+            horas += ucs[cadeira][2]
+    return horas
+
+def verifComp(inscr, ucs):
+    possible = True
+    verif = []
+    for cadeira in inscr:
+        if not possible:                #tentar max eficiencia
+            break
+        else:
+            verif.append(cadeira)       #tentar max eficiencia
+            if cadeira not in ucs.keys():
+                possible = False
+                break
+            else:
+                for cadeira2 in inscr:  #testa compatibilidade cadeira a cadeira
+                    if cadeira2 not in ucs.keys():
+                        possible = False
+                        break
+                    if cadeira2 not in verif:  #tentar max eficiencia
+                        if ucs[cadeira][0] == ucs[cadeira2][0]: # Mesmo Dia
+                            #UC2 acaba antes de UC ou comeca depois
+                            if (ucs[cadeira2][1] + ucs[cadeira2][2] <= ucs[cadeira][1]) or (ucs[cadeira][1] + ucs[cadeira][2] <= ucs[cadeira2][1]):
+                                verif.append(cadeira2)
+                                continue
+                            else:
+                                possible = False
+                                break
+                                
+    return possible
+
+def horario(ucs,alunos):
+    alunosValidos = []
+    
+    for num, inscr in alunos.items():
+        if verifComp(inscr, ucs):
+            alunosValidos.append((num, calcHoras(inscr, ucs)))
+            
+    alunosValidos.sort(key = lambda x: (-x[1],x[0]))
+    return alunosValidos
